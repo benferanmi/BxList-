52d2fdd159b09c18a89c0a8bd71715f4cd1618a1
benjamin <benferanmiopafunso@gmail.com>
2022-10-07 13:52:17 +0100

    adding form to the contact page

--- src/component/reuseable/Caritemlist.js
+++ src/component/reuseable/Caritemlist.js
@@ -1,55 +1,63 @@
-import React from 'react'
-import { useState } from 'react';
+import {
+  faGrip,
+  faGripHorizontal,
+  faGripVertical,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { useState } from "react";
 
-const Caritemlist = ({props, items}) => {
-
-   //styles usestate for grid changing options 
-   const [gridStyle, setgridStyle] = useState("sectionone-submain");
-   const changeStyleOne = () => {
-     setgridStyle("sectionone-submainone")
-   }
-   const changeStyleTwo = () => {
-    setgridStyle("sectionone-submaintwo")
-  }
+const Caritemlist = ({ props, items }) => {
+  //styles usestate for grid changing options
+  const [gridStyle, setgridStyle] = useState("sectionone-submain");
+  const changeStyleOne = () => {
+    setgridStyle("sectionone-submainone");
+  };
+  const changeStyleTwo = () => {
+    setgridStyle("sectionone-submaintwo");
+  };
   const changeStyleThree = () => {
-    setgridStyle("sectionone-submainthree")
-  }
-
+    setgridStyle("sectionone-submainthree");
+  };
 
   return (
     <div>
-      
       <div className={gridStyle}>
-        {items.map(currentItems => (
+        {items.map((currentItems) => (
+          <div key={currentItems.title} className="sectionone">
+            <img
+              src={currentItems.image}
+              alt={`${currentItems.title}/ ${currentItems.class}`}
+            />
+            <span>
+              <h1>{currentItems.title}</h1>
+              <p>{currentItems.class}</p>
+              <p>{currentItems.start_production}</p>
+            </span>
+          </div>
+          // <ul key={currentItems.title}>
+          //  <div className='car-list-li'>
+          //  <li><img src={currentItems.image} alt={currentItems.title + "image"}/></li>
+          //   <li>{currentItems.class}</li>
+          //   <li>{currentItems.discription}</li>
+          //  </div>
+          // </ul>
+        ))}
+      </div>
 
-              <div key={currentItems.title} className="sectionone">
-                <img src={currentItems.image} alt={`${currentItems.title}/ ${currentItems.class}`} />
-                 <span> 
-                  <h1>{currentItems.title}</h1>
-                  <p>{currentItems.class}</p>
-                  <p>{currentItems.start_production}</p>
-                  </span>
-              </div>
-              // <ul key={currentItems.title}>
-              //  <div className='car-list-li'>
-              //  <li><img src={currentItems.image} alt={currentItems.title + "image"}/></li>  
-              //   <li>{currentItems.class}</li>  
-              //   <li>{currentItems.discription}</li>  
-              //  </div>
-              // </ul>
-              
-            ))
-          }    
+      <div className="grid-styles">
+        <button type="button" onClick={changeStyleOne}>
+          one
+        </button>
+        <button type="button" onClick={changeStyleTwo}>
+          <FontAwesomeIcon icon={faGripVertical} />
+        </button>
+        <button type="button" className="button3" onClick={changeStyleThree}>
+          <FontAwesomeIcon icon={faGrip} />
+        </button>
+      </div>
     </div>
+  );
+};
 
-    
-<div className='grid-styles'>
-<button type="button" onClick={changeStyleOne}>one</button>
-<button type="button" onClick={changeStyleTwo}>two</button>
-<button type="button" onClick={changeStyleThree}>three</button>
-</div>
-    </div>
-  )
-}
-
-export default Caritemlist
+export default Caritemlist;
